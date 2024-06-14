b1da7e21db7af75a275fa3dfd043a73490567d9e
Trung <nguyentrung250302@gmail.com>
2024-06-14 20:39:32 +0700

    completed

---
+++ footernguoidung.php
@@ -0,0 +1,69 @@
+<div class="footer">
+            <div class="grid wide">
+                <div class="row">
+                    <div class="col l-5">
+                        <div class="footer-items__first">
+                            <img src="assets/img/logoabc.png" alt="" class="footer-items__logo">
+                            <!-- <a href="#!" class="header__left">
+                                <h1 class="header__left-title">NguyenHuong <span class="header__left-privates">Shop</span></h1>
+                                <span class="header__left-msg">NH<span class="header__left-privates">Shop</span></span>
+                            </a> -->
+                            <p class="footer-items__intro">Shop thể thao và du lịch</p>
+                            <div class="footer-items__address">
+                                <div class="footer-items__address-gr">
+                                    <i class="footer-items__address-icon footer-items__address-icon-strong fa-solid fa-location-dot"></i>
+                                    <p>Toà T4 Thăng Long Capital, An Khánh, Hoài Đức, Hà Nội</p>
+                                </div>
+                                <div class="footer-items__address-gr">
+                                    <i class="footer-items__address-icon fa-regular fa-clock"></i>
+                                    <p>Giờ làm việc: Từ 9:00 đến 22:00 các ngày trong tuần từ Thứ 2 đến Chủ nhật</p>
+                                </div>
+                                <div class="footer-items__address-gr">
+                                    <i class="footer-items__address-icon fa-solid fa-phone"></i>
+                                    <p class="footer-bottom__call-msg-text">
+                                        Số điện thoại:
+                                        <a href="tel:0764 481 906" class="footer-bottom__call-msg-num">0984 69 2509</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col l-7">
+                        <div class="row">
+                            <div class="col l-4">
+                                <div class="footer-items__last">
+                                    <h4 class="footer-items__heading">Về chúng tôi</h4>
+                                    <ul class="footer-items__list">
+                                        <li class="footer-items__info">
+                                            <a href="index_home.php" class="footer-items__info-links">Trang chủ</a>
+                                        </li>
+                                        <div class="footer-items__info">
+                                            <a href="all.php" class="footer-items__info-links">Sản phẩm</a>
+                                        </div>
+                                        <div class="footer-items__info">
+                                            <a href="index_news.php" class="footer-items__info-links">Tin tức</a>
+                                        </div>
+                                        <div class="footer-items__info">
+                                            <a href="contact.php" class="footer-items__info-links">Liên hệ</a>
+                                        </div>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col l-4">
+                                <div class="footer-items__last">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <span class="footer-copyright__text">Nguyễn Hương </span>
+        </div>
+
+    </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
